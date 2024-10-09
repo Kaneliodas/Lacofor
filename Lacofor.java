92b2559ebfdd50a0d17ff8a6551fcb004835dbc2
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-10-09 07:38:18 -0300

    Create Lacofor.java

---
+++ Lacofor.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Lacofor {
+    public static void main(String[] args) {
+      for(int i = 1; i <= 10 ; i++ ) {
+        System.out.println("Contagem: "+ i);
+      }
+  }
+}
